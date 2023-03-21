a791d635136071d8ab8f52920b937fbea2c64701
rahul sharma <109339946+rahul24sharma@users.noreply.github.com>
2023-03-21 18:43:45 +0530

    57

--- src/components/Sidetable/Sidetable.jsx
+++ src/components/Sidetable/Sidetable.jsx
@@ -1,15 +1,29 @@
 import React, { useState } from "react";
 import "./Sidetable.css";
+import ToggleButton from '@mui/material/ToggleButton';
+import ToggleButtonGroup from '@mui/material/ToggleButtonGroup';
 const Sidetable = () => {
-  const [selectedButton, setSelectedButton] = useState("off");
+  const [alignment, setAlignment] = React.useState('web');
 
-  function handleButtonSelection(buttonName) {
-    setSelectedButton(buttonName);
-  }
+  const handleChange = (event, newAlignment) => {
+    setAlignment(newAlignment);
+  };
 
   return (
     <div className="table">
-
+      <div className ='Tog'>
+      <ToggleButtonGroup
+      color="primary"
+      value={alignment}
+      exclusive
+      onChange={handleChange}
+      aria-label="Platform"
+    >
+      <ToggleButton value="web">my bets</ToggleButton>
+      <ToggleButton value="android">top</ToggleButton>
+      <ToggleButton value="ios">All bets</ToggleButton>
+    </ToggleButtonGroup>
+    </div>
     </div>
   );
 };
