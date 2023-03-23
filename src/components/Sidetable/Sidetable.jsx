ba60af1467dbfef3201128c2423a34418615bcec
rahul sharma <109339946+rahul24sharma@users.noreply.github.com>
2023-03-23 18:14:46 +0530

    80

--- src/components/Sidetable/Sidetable.jsx
+++ src/components/Sidetable/Sidetable.jsx
@@ -1,16 +1,32 @@
 import React, { useState } from "react";
 import "./Sidetable.css";
-import ToggleButton from '@mui/material/ToggleButton';
-import ToggleButtonGroup from '@mui/material/ToggleButtonGroup';
+import ToggleButton from "@mui/material/ToggleButton";
+import ToggleButtonGroup from "@mui/material/ToggleButtonGroup";
 const Sidetable = () => {
-  const [alignment, setAlignment] = React.useState('web');
+  const [toggle, setToggle] = useState(false);
 
-  const handleChange = (event, newAlignment) => {
-    setAlignment(newAlignment);
+  const handleToggleChange = () => {
+    setToggle(!toggle);
   };
 
   return (
     <div className="table">
+      <div className="custom-tog">
+      <div className="custom-slider-container">
+              <label className="slid">
+                <div className="slid-label le">Off</div>
+                <input
+                  type="checkbox"
+                  checked={toggle}
+                  onChange={handleToggleChange}
+                />
+                <div className="slid-button">
+                  <div className="slider-button-label onn">All Bet</div>
+                  <div className="slider-button-label offf">My Bet</div>
+                </div>
+              </label>
+            </div>
+      </div>
     </div>
   );
 };
