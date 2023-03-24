aee159af464f8e1ddd9d7f55d6b0a200a4ef9403
rahul sharma <109339946+rahul24sharma@users.noreply.github.com>
2023-03-24 18:39:01 +0530

    101

--- src/components/Canvas/Canvas.jsx
+++ src/components/Canvas/Canvas.jsx
@@ -17,6 +17,7 @@ const Canvas = () => {
     });
     appRef.current = app;
     document.body.appendChild(app.view);
+    app.ticker.maxFPS = 60;
 
     const blackRect = new PIXI.Graphics();
     blackRect.beginFill(0x00000);
