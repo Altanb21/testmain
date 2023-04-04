08a9bc72611ac8114eddcba84eb9c7d148662707
rahul sharma <109339946+rahul24sharma@users.noreply.github.com>
2023-04-04 12:30:10 +0530

    151

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
+import React, { useState, useEffect, Suspense } from 'react';
 import "./index.css";
-// import Canvas from "./components/Canvas/Canvas";
+import Canvas from "./components/Canvas/Canvas";
 import Navbar from "./components/Navbar/Navbar";
-// import Controls from "./components/Controls/Controls";
+import Controls from "./components/Controls/Controls";
 import Login from "./components/Login/Login";
 import Signup from "./components/Signup/Signup";
 import Results from "./components/Results/Results";
-// import Sidetable from "./components/Sidetable/Sidetable";
+import Sidetable from "./components/Sidetable/Sidetable";
 import { Routes, Route } from "react-router-dom";
 import { auth } from "./firebase";
-import { useEffect, useState } from "react";
 import Home from "./components/Home/Home";
-import Loader from "./components/Loader/Loader";
-import React, { lazy, Suspense } from "react";
-const Canvas = lazy(() => import("./components/Canvas/Canvas"));
-const Sidetable = lazy(() => import("./components/Sidetable/Sidetable"));
-const Controls = lazy(() => import("./components/Controls/Controls"));
+import Loader from './components/Loader/Loader'
 function App() {
   const [username, setUsername] = useState("");
-  const [loading, setLoading] = useState(true);
+  const [loading, setLoading] = useState(false);
 
+  useEffect(() => {
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false);
+    }, 2000);
+  }, []);
   useEffect(() => {
     auth.onAuthStateChanged((user) => {
       if (user) {
@@ -32,14 +34,6 @@ function App() {
     auth.signOut();
     setUsername("");
   };
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      setLoading(false);
-    }, 3000);
-
-    return () => clearTimeout(timer);
-  }, []);
-
   return (
     <div>
       <Routes>
@@ -89,6 +83,5 @@ function Play({ handleSignOut, name }) {
     </>
   );
 }
-
 
 export default App;
