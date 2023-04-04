5ba5f29d0aea9e52e865cded0c36f4b0d450b2ed
rahul sharma <109339946+rahul24sharma@users.noreply.github.com>
2023-04-04 11:08:14 +0530

    146

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
 import "./index.css";
-import Canvas from "./components/Canvas/Canvas";
+// import Canvas from "./components/Canvas/Canvas";
 import Navbar from "./components/Navbar/Navbar";
-import Controls from "./components/Controls/Controls";
+// import Controls from "./components/Controls/Controls";
 import Login from "./components/Login/Login";
 import Signup from "./components/Signup/Signup";
 import Results from "./components/Results/Results";
-import Sidetable from "./components/Sidetable/Sidetable";
+// import Sidetable from "./components/Sidetable/Sidetable";
 import { Routes, Route } from "react-router-dom";
 import { auth } from "./firebase"
 import { useEffect, useState } from "react";
 import Home from "./components/Home/Home";
+import Loader from "./components/Loader/Loader";
+import React, { lazy, Suspense } from 'react';
+const Canvas = lazy(() => import('./components/Canvas/Canvas'));
+const Sidetable = lazy(() => import('./components/Sidetable/Sidetable'));
+const Controls = lazy(() => import('./components/Controls/Controls'));
 function App() {
   const [username,setUsername] = useState("")
   useEffect(()=>{
@@ -33,14 +38,16 @@ function App() {
       <Route path="/login" element={<Login name={username} />} />
       <Route path="/results" element={<Results name={username} />} />
       <Route
-        path="/play"
-        element={
-          <>
-            <Navbar handleSignOut={handleSignOut} name={username} />
-            <Canvas />
-            <Sidetable />
-            <Controls name={username} />
-          </>
+  path="/play"
+  element={
+    <>
+      <Navbar handleSignOut={handleSignOut} name={username} />
+      <Suspense fallback={<Loader />}>
+        <Canvas />
+        <Sidetable />
+        <Controls name={username} />
+      </Suspense>
+    </>
         }
       />
     </Routes>
---
+++ src/components/Loader/Loader.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import { Audio } from 'react-loader-spinner'
+import './Loader.css'
+
+
+const Loader = () => {
+  return (
+    <div className='loadermain'>
+      <Audio
+  height="80"
+  width="80"
+  radius="9"
+  color="aqua"
+  ariaLabel="loading"
+  wrapperStyle
+  wrapperClass
+/>
+    </div>
+  )
+}
+
+export default Loader
