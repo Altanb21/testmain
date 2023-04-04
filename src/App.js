003b83a6fe439c0ed93fa4ae60bed7f899bdc637
rahul sharma <109339946+rahul24sharma@users.noreply.github.com>
2023-04-04 11:18:50 +0530

    147

--- src/App.js
+++ src/App.js
@@ -7,51 +7,63 @@ import Signup from "./components/Signup/Signup";
 import Results from "./components/Results/Results";
 // import Sidetable from "./components/Sidetable/Sidetable";
 import { Routes, Route } from "react-router-dom";
-import { auth } from "./firebase"
+import { auth } from "./firebase";
 import { useEffect, useState } from "react";
 import Home from "./components/Home/Home";
 import Loader from "./components/Loader/Loader";
-import React, { lazy, Suspense } from 'react';
-const Canvas = lazy(() => import('./components/Canvas/Canvas'));
-const Sidetable = lazy(() => import('./components/Sidetable/Sidetable'));
-const Controls = lazy(() => import('./components/Controls/Controls'));
+import React, { lazy, Suspense } from "react";
+const Canvas = lazy(() => import("./components/Canvas/Canvas"));
+const Sidetable = lazy(() => import("./components/Sidetable/Sidetable"));
+const Controls = lazy(() => import("./components/Controls/Controls"));
 function App() {
-  const [username,setUsername] = useState("")
-  useEffect(()=>{
-    auth.onAuthStateChanged((user)=>{
-      if(user){
-        setUsername(user.displayName)
-      }else setUsername("")
-    })
-  },[])
+  const [username, setUsername] = useState("");
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    auth.onAuthStateChanged((user) => {
+      if (user) {
+        setUsername(user.displayName);
+      } else setUsername("");
+    });
+  }, []);
 
   const handleSignOut = (e) => {
     e.preventDefault();
     auth.signOut();
     setUsername("");
   };
+
+  useEffect(() => {
+    window.onload = () => {
+      setLoading(false);
+    };
+  }, []);
   return (
-    <>
-    <Routes>
-    <Route path="/" element={<Home />} />
-      <Route path="/signup" element={<Signup />} />
-      <Route path="/login" element={<Login name={username} />} />
-      <Route path="/results" element={<Results name={username} />} />
-      <Route
-  path="/play"
-  element={
-    <>
-      <Navbar handleSignOut={handleSignOut} name={username} />
-      <Suspense fallback={<Loader />}>
-        <Canvas />
-        <Sidetable />
-        <Controls name={username} />
-      </Suspense>
-    </>
-        }
-      />
-    </Routes>
-  </>
+    <div>
+      {loading ? (
+        <Loader />
+      ) : (
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/login" element={<Login name={username} />} />
+          <Route path="/results" element={<Results name={username} />} />
+          <Route
+            path="/play"
+            element={
+              <>
+                <Navbar handleSignOut={handleSignOut} name={username} />
+                <Suspense fallback={<Loader />}>
+                  <Canvas />
+                  <Sidetable />
+                  <Controls name={username} />
+                </Suspense>
+              </>
+            }
+          />
+        </Routes>
+      )}
+    </div>
   );
 }
 
