2defe7aff4be282fb2ec4d9e5a38d5f0b0bf41aa
rahul sharma <109339946+rahul24sharma@users.noreply.github.com>
2023-04-09 12:41:07 +0530

    171

--- src/components/Canvas/Canvas.jsx
+++ src/components/Canvas/Canvas.jsx
@@ -331,7 +331,7 @@ const Canvas = () => {
     }
     function resetGame() {
       // reload the page to restart the game
-      window.location.reload();
+      //window.location.reload();
     }
 
     animatePlane();
